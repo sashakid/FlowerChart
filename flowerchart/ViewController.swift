08e7f554e9132ed8e18191a5ee9d83273880484a
Alexander Yakovlev <kidpost@gmail.com>
2017-05-29 18:17:33 +0300

    swift 3 transition, chart redesign, touches and delegate added

--- flowerchart/ViewController.swift
+++ flowerchart/ViewController.swift
@@ -9,74 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     var flowerChart: FlowerChart!
-    var sizesArray = [Double]()
-    var colorsArray = [UIColor]()
-    var labelsArray = [String]()
-    let totalPetals = 9
     
     @IBOutlet weak var petalCanvas: UIView!
-    @IBAction func refreshFlower(sender: AnyObject) {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        if sizesArray.count > 0 {
-            sizesArray.removeAll()
-            for _ in 0 ..< totalPetals {
-                let size = Double(arc4random_uniform(UInt32(11)))
-                sizesArray.append(size)
-            }
-
-            } else {
-            for _ in 0 ..< totalPetals {
-                let size = Double(arc4random_uniform(UInt32(11)))
-                sizesArray.append(size)
-            }
-        }
+        self.view.backgroundColor = UIColor.darkGray
         
-        flowerChart.setPetalSizes(sizesArray)
-    }
-
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(true)
-        
-        colorsArray.append(UIColor(red: 255/255, green: 233/255, blue: 0/255, alpha: 0.99))
-        colorsArray.append(UIColor(red: 226/255, green: 3/255, blue: 29/255, alpha: 0.99))
-        colorsArray.append(UIColor(red: 255/255, green: 40/255, blue: 51/255, alpha: 0.99))
-        colorsArray.append(UIColor(red: 60/255, green: 220/255, blue: 1/255, alpha: 0.99))
-        colorsArray.append(UIColor(red: 116/255, green: 237/255, blue: 0/255, alpha: 0.99))
-        colorsArray.append(UIColor(red: 164/255, green: 255/255, blue: 4/255, alpha: 0.99))
-        colorsArray.append(UIColor(red: 4/255, green: 225/255, blue: 235/255, alpha: 0.99))
-        colorsArray.append(UIColor(red: 4/255, green: 230/255, blue: 230/255, alpha: 0.99))
-        colorsArray.append(UIColor(red: 4/255, green: 250/255, blue: 225/255, alpha: 0.99))
-        
-        sizesArray.append(1.0)
-        sizesArray.append(10.0)
-        sizesArray.append(1.0)
-        sizesArray.append(1.0)
-        sizesArray.append(10.0)
-        sizesArray.append(1.0)
-        sizesArray.append(1.0)
-        sizesArray.append(10.0)
-        sizesArray.append(1.0)
-        
-        labelsArray.append("First")
-        labelsArray.append("Second")
-        labelsArray.append("Third")
-        labelsArray.append("Fourth")
-        labelsArray.append("Fifth")
-        labelsArray.append("Sixth")
-        labelsArray.append("Seventh")
-        labelsArray.append("Eighth")
-        labelsArray.append("Ninth")
-
-        let flowerChart = FlowerChart(petalCanvas: petalCanvas, totalPetals: totalPetals)
+        let flowerChart = FlowerChart(
+            petalCanvas: petalCanvas,
+            data: [8.6, 0.0, 4.1, 0.0, 9.2, 0.0, 6.0, 0.0, 9.5, 0.0, 7.3, 0.0],
+            labels: ["Title 1", "", "Title 2", "", "Title 3", "", "Title 4", "", "Title 5", "", "Title 6", ""]
+        )
         self.flowerChart = flowerChart
-        flowerChart.drawFlower(colorsArray)
-//        flowerChart.drawCenter(20)
-        flowerChart.setPetalSizes(sizesArray)
-        flowerChart.displayLabels(labelsArray)
-        
+        self.flowerChart.delegate = self
+        flowerChart.drawFlower()
     }
+}
 
+extension ViewController: FlowerChartDelegate {
+    func flowerChartTapped(index: Int) {
+        print(index)
+    }
 }
 
